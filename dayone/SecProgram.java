330e0738bf5b7c30012a45735de28db30760cf44
Unnatimahajan <145320294+Unnatimahajan@users.noreply.github.com>
2023-10-04 12:33:52 +0530

    first commit

---
+++ dayone/SecProgram.java
@@ -0,0 +1,16 @@
+package com.tnsif.dayone;
+
+public class SecProgram {
+		
+		public static void main(String args[])
+		{
+			int firstNumber=10;
+			int secondNumber=20;
+			int result;
+			result=firstNumber+secondNumber;
+			System.out.println("result=" +result);
+
+		
+		}
+	}
+
