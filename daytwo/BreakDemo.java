cbcfa7748b375656d809d832efcc6036c04db422
Unnatimahajan <145320294+Unnatimahajan@users.noreply.github.com>
2023-10-04 21:31:37 +0530

    First Commit

---
+++ daytwo/BreakDemo.java
@@ -0,0 +1,19 @@
+package com.tnsif.daytwo;
+
+public class BreakDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+			for (int i = 5; i < 10; i++) {
+				if (i == 5)
+
+				System.out.println(i);
+				break;
+			}
+
+		}
+
+	}
+
+
